50add52cb709859d9c4f3f1145a0d12c0ca1bae7
Paddy <paddyfedoo@hotmail.com>
2023-08-15 22:46:28 +0100

    characters route and page

---
+++ app/api/journalentries/route.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+import journalentries from '@/json/journalentries.json'
+
+
+export async function GET() {
+    return NextResponse.json({ journalentries: journalentries.data })
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,27 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('http://localhost:3000/api/characters');
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
+  // console.log(data);
+
   return (
     <main className={styles.main}>
       <h1>Dracula</h1>
       <p>by</p>
       <p>Bram Stoker</p>
       <p>How these papers have been placed in sequence will be made manifest in the reading of them. All needless matters have been eliminated, so that a history almost at variance with the possibilities of later-day belief may stand forth as simple fact. There is throughout no statement of past things wherein memory may err, for all the records chosen are exactly contemporary, given from the standpoints and within the range of knowledge of those who made them.</p>
+      {
+        data.characters.map((x) => {
+          return <p key={x.id}>{x.name}</p>
+        })
+      }
     </main>
   )
 }
