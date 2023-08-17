0f31bdcfd428ddb871e880eada4ef42ff160e604
Paddy <paddyfedoo@hotmail.com>
2023-08-17 20:56:08 +0100

    Page JournalEntries

--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-async function getData() {
-  const res = await fetch('http://localhost:3000/api/characters');
+async function getJournalEntries() {
+  const res = await fetch('http://localhost:3000/api/journalentries');
   return res.json();
 }
 
 export default async function Home() {
-  const data = await getData();
-
-  // console.log(data);
+  const entries = await getJournalEntries();
 
   return (
     <main className={styles.main}>
@@ -17,11 +15,16 @@ export default async function Home() {
       <p>by</p>
       <p>Bram Stoker</p>
       <p>How these papers have been placed in sequence will be made manifest in the reading of them. All needless matters have been eliminated, so that a history almost at variance with the possibilities of later-day belief may stand forth as simple fact. There is throughout no statement of past things wherein memory may err, for all the records chosen are exactly contemporary, given from the standpoints and within the range of knowledge of those who made them.</p>
+
       {
-        data.characters.map((x) => {
-          return <p key={x.id}>{x.name}</p>
+        entries.journalentries.map((d) => {
+          return <>
+            <h2 key={d.id}>{d.month} {d.day}</h2>
+            <p key={d.index}>{d.entry}</p>
+          </>
         })
       }
+
     </main>
   )
 }
