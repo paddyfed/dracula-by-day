93e64ca56694437ad5a327404fb3c5acb06971b2
Paddy <paddyfedoo@hotmail.com>
2023-08-17 22:33:35 +0100

    Routes with params

---
+++ app/api/journalentries/[date]/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import journalentries from '@/json/journalentries.json'
+
+
+export async function GET(request, { params }) {
+    const date = params.date;    
+
+    const filtered = journalentries.journalentries.filter(month => month.month.toLowerCase() === date.toLowerCase())
+
+    const data = await NextResponse.json({ journalentries: filtered })
+
+    return data;
+}
