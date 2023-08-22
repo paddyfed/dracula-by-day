05ca6848bfd12d0f920aa0cd1f186dfc07bacbb7
Paddy <paddyfedoo@hotmail.com>
2023-08-22 22:47:18 +0100

    Trying to deploy

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 
 async function getJournalEntries() {
   const today = await getToday();
-  const res = await fetch(`http://localhost:3000/api/journalentries/${today}`);
+  const res = await fetch(`https://www.anapioficeandfire.com/api/characters`);
   return res.json();
 }
 
@@ -26,7 +26,7 @@ export default async function Home() {
       <p>How these papers have been placed in sequence will be made manifest in the reading of them. All needless matters have been eliminated, so that a history almost at variance with the possibilities of later-day belief may stand forth as simple fact. There is throughout no statement of past things wherein memory may err, for all the records chosen are exactly contemporary, given from the standpoints and within the range of knowledge of those who made them.</p>
 
       {
-        entries.journalentries.map((d) => {
+        entries.map((d) => {
           return <>
             <h2 key={d.id}>{d.month} {d.day}</h2>
             <p key={d.index}>{d.entry}</p>
