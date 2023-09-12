dfc881af66cf075d695644499f5f437fec6c5ccb
Paddy <paddyfedoo@hotmail.com>
2023-09-12 21:41:19 +0100

    Tidy old code

--- app/api/characters/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import characters from '@/json/characters.json'
-
-
-export async function GET() {
-    return NextResponse.json({ characters: characters.data })
-}
