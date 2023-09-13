ce33dc6abef547fbd0cd0776931b2c8bad3e370f
Paddy <paddyfedoo@hotmail.com>
2023-09-13 21:18:25 +0100

    Removing other Console.Log entries

--- app/components/entry-text.js
+++ app/components/entry-text.js
@@ -4,16 +4,13 @@ import TelegramEntry from "./telegram-entry"
 
 export default function EntryText({ entry }) {
   if (entry.sourceType === "Letter") {
-    console.log("Letter",entry);
     return <LetterEntry entry={entry} />
   }
-  
+
   if (entry.sourceType === "Telegram") {
-    console.log("Telegram", entry);
     return <TelegramEntry entry={entry} />
   }
 
-  console.log("Journal", entry);
   return <JournalEntry entry={entry} />
 
 }
