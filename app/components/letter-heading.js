9309795c34578432e4871c66b06929206250ec43
Paddy <paddyfedoo@hotmail.com>
2023-09-13 21:07:35 +0100

    Found extra Console.Log lurking

--- app/components/letter-heading.js
+++ app/components/letter-heading.js
@@ -1,8 +1,6 @@
 import styles from './letter-heading.module.css';
 
 export default function LetterHeading({heading}) {
-    console.log(heading);
-
     return <section className={styles.headings}>
         {heading?.map((entry, index) => {
             return <p key={index}>{entry}</p>
