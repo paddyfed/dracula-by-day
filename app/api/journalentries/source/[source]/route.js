65ed8eecf55628b6a729a437b16fa0baa29089ad
Paddy <paddyfedoo@hotmail.com>
2023-09-21 22:20:41 +0100

    Sort source entries by month and day

--- app/api/journalentries/source/[source]/route.js
+++ app/api/journalentries/source/[source]/route.js
@@ -1,12 +1,27 @@
 import { NextResponse } from "next/server";
 import journalentries from "@/json/journalentries.json";
+import { getParamMonthNumber } from "@/app/utils/dateHelpers";
 
 export async function GET(request, { params }) {
   const source = params.source;
 
-  const filtered = journalentries.journalentries.filter(
-    (entry) => `${entry.source.toLowerCase()}` === source.toLowerCase(),
-  );
+  const filtered = journalentries.journalentries
+    .filter((entry) => `${entry.source.toLowerCase()}` === source.toLowerCase())
+    .sort((a, b) => {
+      if (getParamMonthNumber(a.month) > getParamMonthNumber(b.month)) {
+        return 1;
+      }
+      if (getParamMonthNumber(a.month) < getParamMonthNumber(b.month)) {
+        return -1;
+      }
+      if (+a.day > +b.day) {
+        return 1;
+      }
+      if (+a.day < +b.day) {
+        return -1;
+      }
+      return 0;
+    });
 
   const data = await NextResponse.json({ journalentries: filtered });
 
